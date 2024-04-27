c1728882ab7da8304bf0e08101b913ce523c6b37
ASHUTOSH10F <117303799+ASHUTOSH10F@users.noreply.github.com>
2024-04-28 00:30:54 +0530

    push

--- src/Components/header/header2.js
+++ src/Components/header/header2.js
@@ -6,7 +6,7 @@ export default function header2() {
       <ul>
         <li><a href='counter'>Counter</a></li>
         <li><a href='/'>Home</a></li>
-        <li><a href='/contact-us'>Contact us</a></li>
+        <li><a href='/contact-us'>Contact</a></li>
       </ul>
     </header>
   )
